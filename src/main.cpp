07b9eb71d269b4ce9d3d1ff57238adea02e250b4
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-04-16 17:14:34 +0200

    run build folder executable list enviroment on execution

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
+#include <cstdlib>
 #include <iostream>
 
 #include "test.pb.h"
 
-int main( int argc, char** argv ) {
+int main( int argc, char** argv, char** envp ) {
+  std::cout << "env:" << std::endl;
+  for( char** env = envp; *env != 0; env++ ) {
+    std::cout << "=> '" << ( *env ) << "'" << std::endl;
+  }
+
+  if( const char* pathEnv = std::getenv( "PATH" ) ) {
+    std::cout << "Your PATH is: " << pathEnv << std::endl;
+  }
+
   Test::Proto::Message* myMessage = new Test::Proto::Message();
   myMessage->set_name( "myMessage->set_name" );
   myMessage->set_string( "myMessage->set_string" );
